685e03f049f936157e912f70869fd5bd9e766ef8
Billa_Firza <68726639+Nabila-JF@users.noreply.github.com>
2021-12-29 19:49:36 +0700

    Variabel dan Tata Cara Penamaannya

---
+++ 01 -- Variabel/src/Main.py
@@ -0,0 +1,34 @@
+# Variabel adalah tempat untuk menyimpan data
+
+# menaru / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# aturan penamaan variabel 
+nilai_y = 3 # memakai underscore
+juta10 = 10000000 # tidak boleh menaruh angka di depan penamaan
+nilaiZ = 8.5 # ini boleh 
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a) # nilai a yang akan terbaca adalah pembaharuan nilai di bawah, bukan di atas
+
+#assignment indirect
+b = a
+print("Nilai b = ", b)
+
+
+
+"""
+Note tambahan:
+Gunakan Alt+Shift+PgDn untuk mencopy paste code dari baris yang dipilih
+Gunakan Alt+PgDn setelah memilih baris yang akan dipindahkan ke bawah
+Gunakan Ctrl+D setelah menyeleksi tulisan yang sama dalam sebaris untuk mengubah tulisan tsb
+Arahkan kursor pada variabel maka kamu akan bisa melihat jenis variabel tsb
+"""
