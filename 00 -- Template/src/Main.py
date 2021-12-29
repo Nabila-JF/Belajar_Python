034fc18caabf5001189521dafcc41b594330e3e7
Billa_Firza <68726639+Nabila-JF@users.noreply.github.com>
2021-12-29 15:32:18 +0700

    Pengenalan Python

---
+++ 00 -- Template/src/Main.py
@@ -0,0 +1,22 @@
+print("halo dunia!!!!")
+print("apa kabar?")
+print ("Hey,")
+print ("Ga jadi.")
+
+# ini comment
+# btw baris kosong ga bisa di eksekusi alias langsung di lewatin
+# python itu bacanya perbaris, jangan lupa ga ada titik koma
+"""
+Ini namanya multiple comment. Tandanya itu
+pake tanda petik (") tiga kali di awal dan di akhir.
+"""
+
+a = 10
+print (a) #ini juga comment ngab
+
+# Untuk mempercepat proses ekesekusi program python 
+# Kamu bisa mengetik ini untuk mengcompile pyhthon menjadi bytecode
+# python -m py_compile (nama filenya)
+# dijalankan dengan mengetik perintah
+# python __pychace__/Main.cpython-38.pyc
+# Tapi berhubung di laptopku ga bisa, yaudah
